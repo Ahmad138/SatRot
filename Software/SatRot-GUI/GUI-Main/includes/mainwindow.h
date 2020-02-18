60d9187e66eddcbf30a31c3cb719e55103c02dcf
Ahmad Muhammad <ahmad.muhd13@hotmail.com>
2020-02-18 18:36:06 +0000

    Started with the basic GUI design

---
+++ Software/SatRot-GUI/GUI-Main/includes/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <string>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_horizontalSlider_valueChanged(int value);
+
+
+    void on_verticalSlider_valueChanged(int value);
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
--- Software/SatRot/SatRot.pro
+++
@@ -1,5 +0,0 @@
-TEMPLATE = subdirs
-
-SUBDIRS += \
-    SatRot_Main \
-    apiTest
