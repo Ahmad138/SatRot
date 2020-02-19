830644cd1135e1b196ea4ed3f2339480e391f571
Ahmad Muhammad <ahmad.muhd13@hotmail.com>
2020-02-19 10:28:22 +0000

    worked on the new rotorDriverPi class

---
+++ Software/RotorDriverPi/include/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "a4988Driver.h"
+#include "helper.h"
+
+class main
+{
+    public:
+        main();
+
+    protected:
+
+    private:
+};
+
+#endif // MAIN_H
